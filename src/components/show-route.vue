7c921e2ee5b744933c3c0449d7e02d8e5478651c
osennyaya <supersduby@gmail.com>
2021-04-03 08:54:32 +0800

    ✨feat(component.show-route): 添加显示路线信息模块

---
+++ src/components/show-route.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-tabs class="route-tabs" type="border-card" tab-position="top">
+    <el-tab-pane v-for="item in panes" :key="item.name">
+      <template #label>
+        <span><i :class="item.icon"></i> {{ item.name }}</span>
+      </template>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+export default {
+  name: "show-route",
+  setup() {
+    const panes = [
+      {
+        name: "步行",
+        icon: "el-icon-user",
+      },
+      {
+        name: "骑车",
+        icon: "el-icon-bicycle",
+      },
+      {
+        name: "公交",
+        icon: "el-icon-truck",
+      },
+    ];
+    return {
+      panes,
+    };
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.route-tabs {
+  width: 350px;
+}
+</style>
