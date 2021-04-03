7defd4bc393c82918be36be5c9dc290a61d85dec
osennyaya <supersduby@gmail.com>
2021-04-03 09:41:38 +0800

    ✨feat(component.show-route): 完善了路线展示组件
    现在可以选择隐藏, 避免遮挡用户视线

--- src/components/show-route.vue
+++ src/components/show-route.vue
@@ -1,11 +1,16 @@
 <template>
-  <el-tabs class="route-tabs" type="border-card" tab-position="top">
-    <el-tab-pane v-for="item in panes" :key="item.name">
-      <template #label>
-        <span><i :class="item.icon"></i> {{ item.name }}</span>
-      </template>
-    </el-tab-pane>
-  </el-tabs>
+  <div :class="{'route-tabs-parent':true,'route-tabs-parent-collapsed':collapsed}">
+    <el-tabs class="route-tabs" type="border-card" tab-position="top">
+      <el-tab-pane v-for="item in panes" :key="item.name">
+        <template #label>
+          <span><i :class="item.icon"></i> {{ item.name }}</span>
+        </template>
+      </el-tab-pane>
+    </el-tabs>
+  <div class="close-btn">
+    <el-button plain :icon="hideIcon" @click="hide()" size="small"></el-button>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -31,15 +36,37 @@ export default {
     };
   },
   data() {
-    return {};
+    return {
+      collapsed: false,
+      hideIcon: 'el-icon-arrow-left',
+    };
   },
   computed: {},
-  methods: {},
+  methods: {
+    hide() {
+      this.collapsed=!this.collapsed
+      this.hideIcon=this.collapsed?'el-icon-arrow-right':'el-icon-arrow-left'
+    }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.route-tabs {
+.route-tabs-parent {
   width: 350px;
+  position: relative;
+  text-align: left;
+  transition-duration: 200ms;
+}
+.route-tabs-parent-collapsed {
+  transform: translateX(-350px);
+  transition-duration: 200ms;
+}
+.route-tabs {
+}
+.close-btn {
+  position: absolute;
+  right: -45px;
+  top: 0px;
 }
 </style>
