822c2ad8d1a9ca548ea07cfd51ad0c91b9ca7a29
osennyaya <supersduby@gmail.com>
2021-04-04 15:51:13 +0800

    ✨feat(component.bupt-map): 添加校区切换系统,增加沙河校区图片

--- src/components/bupt-map.vue
+++ src/components/bupt-map.vue
@@ -15,23 +15,33 @@
     </canvas>
   </div>
   <div class="block">
-    <el-slider
-      v-model="slidescale"
-      @change="sliderupdate"
-      :min="minScale"
-      :max="maxScale"
-      :step="0.05"
-      vertical
-      height="200px"
-    >
-    </el-slider>
+    <el-card shadow="always" :body-style="{padding: '10px 0'}">
+      <div style="position: relative" class="slider">
+        <el-slider
+          v-model="slidescale"
+          @change="sliderupdate"
+          :min="minScale"
+          :max="maxScale"
+          :step="0.05"
+          vertical
+          height="200px"
+        > </el-slider>
+        <i style="position:absolute;top:10px;left:-10px" class="el-icon-plus"></i>
+        <i style="position:absolute;bottom:10px;left:-10px" class="el-icon-minus"></i>
+      </div>
+    </el-card>
+      <div class="button">
+        <el-button type="primary" size="medium" @click="switchButton"
+          >切换到{{ button }}</el-button
+        >
+      </div>
   </div>
   <!-- <div class="center">{{ dpr }}</div> -->
 </template>
 
 <script>
 import buptimg from "../assets/bupt.png";
-import logo from "../assets/logo.png";
+import shahe from "../assets/bupt2.png";
 
 export default {
   props: {
@@ -45,6 +55,9 @@ export default {
     this.img1.onload = () => {
       this.draw();
     };
+    this.img2.onload = () => {
+      console.log("2");
+    };
     this.canvasResize();
     window.onresize = () => {
       this.canvasResize();
@@ -52,12 +65,12 @@ export default {
     };
   },
   setup() {
-    const minScale = 0.3
-    const maxScale = 1.4
+    const minScale = 0.3;
+    const maxScale = 1.4;
     let img1 = new Image();
     let img2 = new Image();
     img1.src = buptimg;
-    img2.src = logo;
+    img2.src = shahe;
     return {
       img1,
       img2,
@@ -70,32 +83,49 @@ export default {
       slidescale: 1,
       scale: 1,
       dpr: window.devicePixelRatio,
+      mainImg: { name: "" },
+      button: "沙河校区",
       imgs: [
         {
+          name: "本部",
           lastcoords: [0, 0],
           newcoords: [0, 0],
           img: this.img1,
         },
         {
+          name: "沙河校区",
           lastcoords: [100, 100],
           newcoords: [100, 100],
           img: this.img2,
         },
       ],
-      lastclick: [0,0]
+      lastclick: [0, 0],
     };
   },
   methods: {
+    switchButton() {
+      this.switchMap(this.button);
+      this.button = this.button === "本部" ? "沙河校区" : "本部";
+    },
+    switchMap(name) {
+      for (let img of this.imgs) {
+        if (img.name === name) {
+          this.mainImg = img;
+          img.newcoords = img.lastcoords;
+          this.draw();
+          break;
+        }
+      }
+    },
     zoom(center, scale) {
       let img = this.mainImg;
-      console.log(center);
       img.newcoords[0] = (-center[0] + img.lastcoords[0]) / this.scale;
       img.newcoords[1] = (-center[1] + img.lastcoords[1]) / this.scale;
       this.scale = scale;
       img.newcoords[0] *= scale;
       img.newcoords[1] *= scale;
       this.ctx.save();
-      this.ctx.setTransform(1,0,0,1,0,0)
+      this.ctx.setTransform(1, 0, 0, 1, 0, 0);
       this.ctx.translate(center[0], center[1]);
       this.draw();
       this.ctx.restore();
@@ -109,7 +139,6 @@ export default {
       let scale = this.scale + (event.deltaY < 0 ? 0.05 : -0.05);
       if (scale > this.maxScale) scale = this.maxScale;
       else if (scale < this.minScale) scale = this.minScale;
-      console.log(offset);
       this.zoom(offset, scale);
     },
     logStart(e) {
@@ -130,10 +159,10 @@ export default {
         let offset = [];
         if (e.touches) offset = [e.touches[0].pageX, e.touches[0].pageY];
         else offset = [e.offsetX, e.offsetY];
-          let delta = this.toRelative(offset, this.last);
-          this.last = offset;
-          this.ctx.translate(delta[0], delta[1]);
-          this.draw();
+        let delta = this.toRelative(offset, this.last);
+        this.last = offset;
+        this.ctx.translate(delta[0], delta[1]);
+        this.draw();
       }
     },
     logEnd(e) {
@@ -144,8 +173,7 @@ export default {
         let arr = this.toRelative(this.last, this.start);
         img.lastcoords[0] += arr[0];
         img.lastcoords[1] += arr[1];
-        if (!e.touches || e.touches.length===0)
-        this.start = null;
+        if (!e.touches || e.touches.length === 0) this.start = null;
         this.ctx.restore();
       }
     },
@@ -168,6 +196,8 @@ export default {
       this.clearCanvas();
       let img = this.mainImg;
       let scale = this.scale || 1;
+      console.log('new ', img.newcoords);
+      console.log('scale ', this.scale);
       this.ctx.drawImage(
         img.img,
         img.newcoords[0],
@@ -179,16 +209,15 @@ export default {
       this.ctx.fillRect(nw[0], nw[1], 10, 10);
       if (this.start) {
         let c = this.toCanvas([
-          (this.start[0] - this.mainImg.lastcoords[0])/this.scale,
-          (this.start[1] - this.mainImg.lastcoords[1])/this.scale
+          (this.start[0] - this.mainImg.lastcoords[0]) / this.scale,
+          (this.start[1] - this.mainImg.lastcoords[1]) / this.scale,
         ]);
         this.ctx.fillRect(c[0], c[1], 10, 10);
       }
     },
     sliderupdate() {
-      console.log(this.lastclick);
       this.zoom(this.lastclick, this.slidescale);
-      this.slidescale = this.scale
+      this.slidescale = this.scale;
     },
     toCanvas([x, y]) {
       let nw = this.mainImg.newcoords;
@@ -199,7 +228,7 @@ export default {
       return [x1 - x2, y1 - y2];
     },
     getDistance(p1, p2) {
-      return Math.sqrt(Math.pow((p1[0] - p2[0]), 2) + Math.pow((p1[1] - p2[1]), 2));
+      return Math.sqrt(Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2));
     },
   },
 };
@@ -221,8 +250,21 @@ export default {
   right: 10vh;
 }
 .block {
+  width: 60px;
+  display:flex;
+  flex-direction: column;
+  :deep(.el-card__body) {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    flex-direction: column;
+  };
   position: fixed;
   bottom: 40px;
   right: 20px;
+}
+.button {
+  align-self: flex-end;
+  margin-top: 20px;
 }
 </style>
