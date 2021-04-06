a96e4b57995d9d641bb9a4e2e517e2a39e229ddb
osennyaya <supersduby@gmail.com>
2021-04-06 18:47:10 +0800

    ✨feat(bupt-map): 测试图片预加载

--- src/components/bupt-map.vue
+++ src/components/bupt-map.vue
@@ -1,33 +1,98 @@
 <template>
-  <div>
-    <img
-      class="map_img"
-      src="../assets/bupt.png"
-      alt="bupt"
-      ondragstart="return false;"
-      oncontextmenu="return false;"
-      @touchmove="showtouch"
-    />
+  <div id="map-div">
+    <canvas id="map-canvas" />
   </div>
   <div class="center">
-    {{ touch }} 
+    {{ touch }}
   </div>
 </template>
 
 <script>
+import "@interactjs/auto-start";
+import "@interactjs/actions/drag";
+import "@interactjs/actions/resize";
+import "@interactjs/actions/gesture";
+import "@interactjs/modifiers";
+import "@interactjs/dev-tools";
+import interact from "@interactjs/interact";
+import { onMounted } from "@vue/runtime-core";
+import bupt1 from "../assets/bupt.png";
+
 export default {
-  name: 'bupt-map',
-  methods: {
-    showtouch(event) {
-      this.touch=event.touches[0].pageX
+  name: "bupt-map",
+  setup() {
+    var canvas = null;
+    var ctx = null;
+
+    class canvasImage {
+      constructor(src, x = 0, y = 0, scale = 1) {
+        this.img = new Image();
+        this.load = new Promise((resolve, reject) => {
+          this.img.onload = () => {
+            resolve('ok')
+          }
+          this.img.onerror = () => {
+            reject('error')
+          }
+        })
+        this.x = x;
+        this.y = y;
+        this.scale = scale;
+        this.img.src = src;
+      }
+      async draw() {
+        let scale = this.scale;
+        await this.load
+        ctx.drawImage(
+          this.img,
+          this.x,
+          this.y,
+          this.img.width * scale,
+          this.img.height * scale
+        );
+      }
     }
+
+    var bgImg = new canvasImage(bupt1);
+
+    const loadcanvas = () => {
+      canvas = document.getElementById("map-canvas");
+      ctx = canvas.getContext("2d");
+      canvasResize();
+      window.onresize = canvasResize;
+    };
+
+    const canvasResize = () => {
+      let dpr = window.devicePixelRatio;
+      canvas.height = window.innerHeight * dpr;
+      canvas.width = window.innerWidth * dpr;
+      canvas.style.height = window.innerHeight;
+      canvas.style.width = window.innerWidth;
+      draw()
+    };
+
+    const draw = () => {
+      bgImg.draw();
+    };
+
+    onMounted(loadcanvas);
+
+    return {
+      canvasResize,
+      canvas,
+      ctx,
+    };
   },
+  mounted() {
+    interact;
+  },
+  methods: {},
   data() {
     return {
-      touch:'',
-    }
-  }
-}
+      touch: "",
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
