570dde262dd0afbfd0ac4c14fea5707a90d28db6
osennyaya <supersduby@gmail.com>
2021-04-07 10:48:47 +0800

    feat(bupt-map): 添加背景图片/marker/方向图片类
    实现绘制方法, marker坐标映射至底部, 方向箭头坐标映射至中间,
    实现方向箭头转向绘制

--- src/components/bupt-map.vue
+++ src/components/bupt-map.vue
@@ -17,6 +17,8 @@ import "@interactjs/dev-tools";
 import interact from "@interactjs/interact";
 import { onMounted } from "@vue/runtime-core";
 import bupt1 from "../assets/bupt.png";
+import markerimg from "../assets/marker.png";
+import directionimg from "../assets/direction.png";
 
 export default {
   name: "bupt-map",
@@ -35,25 +37,91 @@ export default {
             reject('error')
           }
         })
-        this.x = x;
-        this.y = y;
+        this._x = x;
+        this._y = y;
         this.scale = scale;
         this.img.src = src;
+      }
+      get x() {
+        return this._x
+      }
+      get y() {
+        return this._y
       }
       async draw() {
         let scale = this.scale;
         await this.load
         ctx.drawImage(
           this.img,
-          this.x,
-          this.y,
+          this._x,
+          this._y,
           this.img.width * scale,
           this.img.height * scale
         );
+        return this.load
+      }
+    } 
+
+    class DirectionImg extends canvasImage {
+      constructor(src, x = 0, y = 0, scale = 1, angle = 0) {
+        super(src, x, y, scale) 
+        this.x = x
+        this.y = y
+        this.angle = angle
+      }
+      get x() {
+        return this._x + this.img.width/2 * this.scale
+      }
+      get y() {
+        return this._y + this.img.height/2 * this.scale
+      }
+      set x(x) {
+        this._x = x - this.img.width/2 * this.scale
+      }
+      set y(y) {
+        this._y = y - this.img.height/2 * this.scale 
+      }
+      async draw() {
+        let scale = this.scale;
+        await this.load
+        ctx.save()
+        ctx.translate(this.x, this.y)
+        ctx.rotate(this.angle)
+        ctx.drawImage(
+          this.img,
+          -0.5*this.img.width * scale,
+          -0.5*this.img.height * scale,
+          this.img.width * scale,
+          this.img.height * scale
+        );
+        ctx.restore()
+        return this.load
       }
     }
 
-    var bgImg = new canvasImage(bupt1);
+    class MarkerImg extends canvasImage {
+      constructor(src, x = 0, y = 0, scale = 1) {
+        super(src, x, y, scale) 
+        this.x = x
+        this.y = y
+      }
+      get x() {
+        return this._x + this.img.width/2*this.scale
+      }
+      get y() {
+        return this._y + this.img.height * this.scale
+      }
+      set x(x) {
+        this._x = x - this.img.width/2*this.scale
+      }
+      set y(y) {
+        this._y = y - this.img.height * this.scale 
+      }
+    }
+
+    var bg = new canvasImage(bupt1);
+    var marker = new MarkerImg(markerimg, 500, 500, 0.04)
+    var direction = new DirectionImg(directionimg, 500, 600, 0.04, 0)
 
     const loadcanvas = () => {
       canvas = document.getElementById("map-canvas");
@@ -68,11 +136,14 @@ export default {
       canvas.width = window.innerWidth * dpr;
       canvas.style.height = window.innerHeight;
       canvas.style.width = window.innerWidth;
+      ctx.imageSmoothingEnabled = true;
       draw()
     };
 
-    const draw = () => {
-      bgImg.draw();
+    const draw = async () => {
+      await bg.draw();
+      await marker.draw()
+      await direction.draw()
     };
 
     onMounted(loadcanvas);
